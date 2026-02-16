23db92a93c6513112e79757f540d8182e3cec45d
Bigyan Raj Shrestha <bigyanrajshrestha@Bigyans-MacBook-Air.local>
2026-02-16 14:37:52 +0545

    api framework samma ko commit/main-commit

---
+++ Booking_backend/Room_booking/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Room(models.Model):
+    ROOM_TYPES = [
+        ('suite', 'Suite'),
+        ('deluxe', 'Deluxe Room'),
+        ('standard', 'Standard Room')
+    ]
+
+    CURRENCY_TYPES = [
+        ('USD', 'US Dollar'),
+        ('NPR', 'Nepalese Rupee')
+    ]
+
+    name = models.CharField(max_length=100, blank=True, default='')
+    type = models.CharField(max_length=100, choices=ROOM_TYPES)
+    pricePerNight = models.IntegerField(default=150)
+    currency = models.CharField(default='NPR', max_length=10, choices=CURRENCY_TYPES)
+    maxOccupancy = models.IntegerField(default=1)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return f"{self.name}  ({self.type})"
